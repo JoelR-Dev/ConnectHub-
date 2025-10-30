8c9f96b4857b9283c6ac9a39cfd58b86892cf944
Jesús Franco <jdfrancov2020@gmail.com>
2025-10-29 22:08:55 -0500

    Agregar configuración inicial del servidor con Express y conexión a la base de datos

--- back/index.js
+++ back/index.js
@@ -1,3 +1,26 @@
-//npm install express dotenv mysql2
-//npm install --save-dev nodemon
-//index.js
+import express from "express";
+import cors from "cors";
+import pool from "./db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", async(req, res)=>{
+    try{
+        const [rows] = await pool.query("SELECT 1 + 1 AS result");
+        res.status(200).json({ message: "OK", db : rows[0]})
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ error: "DB connection failed", details: error.message  })
+    }
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is runnig on http://localhost:${PORT}`)
+})
